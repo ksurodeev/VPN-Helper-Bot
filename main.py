79c8370db806dc1a800c4a11cf87eb05075135dc
Konstantin Surodeev <ksurodeev@gmail.com>
2023-06-17 14:27:10 +0100

    try to execute

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from trafficstatus import Traffic_Status
 
 if __name__ == '__main__':
     check_status = Check_Status('ipsec-vpn-server')
+    check_status.Execute()
     if check_status != 'running':
         pass
     check_logs = Check_Logs('ipsec-vpn-server')
-    Traffic_Status('ipsec-vpn-server')
+    check_logs.Execute()
+    traffic_status = Traffic_Status('ipsec-vpn-server')
+    traffic_status.Execute()
