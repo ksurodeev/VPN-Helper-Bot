a0be299519df86b000c494c28c569e7453591b09
Konstantin Surodeev <ksurodeev@gmail.com>
2023-12-27 23:04:52 +0000

    Add 'show log"

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ if __name__ == '__main__':
     if check_status.Execute() != 'running':
         print('Your container is not in running status')  # here we need to LOG
     check_logs = Check_Logs('ipsec-vpn-server')
-#   check_logs.Execute()
+    check_logs.Execute()
     traffic_status = Traffic_Status('ipsec-vpn-server')
     print(traffic_status.represent_results())
