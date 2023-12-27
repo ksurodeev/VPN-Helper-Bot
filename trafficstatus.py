ff4a32f6ecd06f302855914629b3ecf635b641f6
Konstantin Surodeev <ksurodeev@gmail.com>
2023-12-27 15:28:10 +0000

    Exception handling added when server has no any customers

--- trafficstatus.py
+++ trafficstatus.py
@@ -25,71 +25,75 @@ class Traffic_Status(Command):
             return None
 
     def represent_results(self: str) -> tabulate:
-        regex_customer_type = re.compile(
-            r'.* #[\d]+: (?P<cust_type>\".*\").*'
-        )
-        regex_ike = re.compile(
-            r".* #[\d]+: .*] (?P<ext_ip>[\d.]+), .*,"
-            r" add_time=(?P<start_time>\d+),"
-            r" inBytes=(?P<ingress_bytes>\d+),"
-            r" outBytes=(?P<egress_bytes>\d+),"
-            r" .*"
-            r" id='(?P<username>.*),"
-            r" lease=(?P<int_ip>[\d.]+/\d+)"
-        )
-        # regex_xauth = re.compile(
-        #     r".* #[\d]+: .*] (?P<ext_ip>[\d.]+),"
-        #     r" username=(?P<username>.*), .*,"
-        #     r" add_time=(?P<start_time>\d+),"
-        #     r" inBytes=(?P<ingress_bytes>\d+),"
-        #     r" outBytes=(?P<egress_bytes>\d+),"
-        #     r" lease=(?P<int_ip>[\d.]+\/\d+)"
-        # )
-        for match in regex_customer_type.finditer(
-                self.Execute()
-        ):
-            customers_types = match.groups()
-        for each in customers_types:
-            # if 'ike' in each:
-            ike_customers = []
-            for match in regex_ike.finditer(self.Execute()):
-                ike_customers.append(
-                    [
-                        match.group('ext_ip'),
-                        datetime.fromtimestamp(
-                            int(match.group('start_time')), timezone.utc
-                        ).strftime('%d.%m.%Y %H:%M:%S'),
-                        match.group('ingress_bytes'),
-                        match.group('egress_bytes'),
-                        match.group('username'),
-                        match.group('int_ip')
+        try:
+            regex_customer_type = re.compile(
+                r'.* #[\d]+: (?P<cust_type>\".*\").*'
+            )
+            regex_ike = re.compile(
+                r".* #[\d]+: .*] (?P<ext_ip>[\d.]+), .*,"
+                r" add_time=(?P<start_time>\d+),"
+                r" inBytes=(?P<ingress_bytes>\d+),"
+                r" outBytes=(?P<egress_bytes>\d+),"
+                r" .*"
+                r" id='(?P<username>.*),"
+                r" lease=(?P<int_ip>[\d.]+/\d+)"
+            )
+            # regex_xauth = re.compile(
+            #     r".* #[\d]+: .*] (?P<ext_ip>[\d.]+),"
+            #     r" username=(?P<username>.*), .*,"
+            #     r" add_time=(?P<start_time>\d+),"
+            #     r" inBytes=(?P<ingress_bytes>\d+),"
+            #     r" outBytes=(?P<egress_bytes>\d+),"
+            #     r" lease=(?P<int_ip>[\d.]+\/\d+)"
+            # )
+            for match in regex_customer_type.finditer(
+                    self.Execute()
+            ):
+                customers_types = match.groups()
+            for each in customers_types:
+                # if 'ike' in each:
+                ike_customers = []
+                for match in regex_ike.finditer(self.Execute()):
+                    ike_customers.append(
+                        [
+                            match.group('ext_ip'),
+                            datetime.fromtimestamp(
+                                int(match.group('start_time')), timezone.utc
+                            ).strftime('%d.%m.%Y %H:%M:%S'),
+                            match.group('ingress_bytes'),
+                            match.group('egress_bytes'),
+                            match.group('username'),
+                            match.group('int_ip')
+                        ]
+                    )
+                return tabulate(
+                    ike_customers,
+                    headers=[
+                        'Ext. IP',
+                        'Start Time, UTC',
+                        'inBytes',
+                        'outBytes',
+                        'Username',
+                        'Int. IP'
                     ]
                 )
-            return tabulate(
-                ike_customers,
-                headers=[
-                    'Ext. IP',
-                    'Start Time, UTC',
-                    'inBytes',
-                    'outBytes',
-                    'Username',
-                    'Int. IP'
-                ]
-            )
-            # else:
-            #    xauth_customers = [
-            #        match.groups() for match in regex_xauth.finditer(
-            #            self.Execute()
-            #        )
-            #    ]
-            #    return tabulate(
-            #        xauth_customers,
-            #        headers=[
-            #            'Ext. IP',
-            #            'Username',
-            #            'Start Time',
-            #            'inBytes',
-            #            'outBytes',
-            #            'Int. IP'
-            #        ]
-            #    )
+                # else:
+                #    xauth_customers = [
+                #        match.groups() for match in regex_xauth.finditer(
+                #            self.Execute()
+                #        )
+                #    ]
+                #    return tabulate(
+                #        xauth_customers,
+                #        headers=[
+                #            'Ext. IP',
+                #            'Username',
+                #            'Start Time',
+                #            'inBytes',
+                #            'outBytes',
+                #            'Int. IP'
+                #        ]
+                #    )
+        except UnboundLocalError:
+            print('You have no any customers on your server')
+            return None
